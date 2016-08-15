a41aafa05fef823aa9477ec44537b7309ffdb491
Daniel Zvir <zvir.daniel@gmail.com>
2016-08-15 14:04:19 +0200

    Correct app handling when user closes the app is implemented, default location was removed, users are forced to enter a location.

--- src/main/java/org/weather/view/Controller.java
+++ src/main/java/org/weather/view/Controller.java
@@ -1,6 +1,5 @@
 package org.weather.view;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -18,10 +17,6 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    private CheckMenuItem defaultApiKey;
-    @FXML
-    private CheckMenuItem userApiKey;
     @FXML
     private ImageView weatherIcon;
     @FXML
@@ -49,57 +44,57 @@ public class Controller implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String country = null;
+        String state = null;
         String city = null;
 
         setDefaultApiKey();
 
-        Optional<Pair<String, String>> locationData = userInputDialog();
+        Optional<Pair<String, String>> locationData;
+        WundergroundProvider wunderground;
 
-        if (locationData.isPresent()) {
-            country = locationData.get().getKey();
-            city = locationData.get().getValue();
-        }
-
-        // Downloads all the data from the internet
         while (true) {
+            locationData = userInputDialog();
+
+            if (locationData.isPresent()) {
+                state = locationData.get().getKey().trim();
+                city = locationData.get().getValue().trim();
+            } else {
+                System.exit(0);
+            }
+
             try {
-                WundergroundProvider wunderground = new WundergroundProvider(currentKey, country, city);
+                wunderground = new WundergroundProvider(currentKey, state, city);
                 showWeatherData(wunderground);
                 break;
             } catch (IllegalArgumentException e) {
-                basicUtils.showError("location", "The location was not found.\nDefault location is set.");
-                city = "Prague";
-                country = "CZ";
+                basicUtils.showError("location", "The location was not found.");
             }
         }
     }
 
     private Optional<Pair<String, String>> userInputDialog() {
-        // Create the custom dialog.
         Dialog<Pair<String, String>> dialog = new Dialog<>();
         dialog.setTitle("Select location");
-        dialog.setHeaderText("Please, select your location");
+        dialog.setHeaderText("Please, enter your location:");
 
-        // Set the icon (must be included in the project).
-        dialog.setGraphic(new ImageView(getClass().getResource("/images/MainAppIcon.png").toString()));
 
-        // Set the button types.
         ButtonType loginButtonType = new ButtonType("Confirm", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+        dialog.getDialogPane().getButtonTypes().add(loginButtonType);
 
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(20, 150, 10, 10));
 
-        TextField country = new TextField();
-        country.setPromptText("GB / FL / CZ");
+        TextField state = new TextField();
+        state.setPromptText("GB / FL / CZ");
+
         TextField city = new TextField();
         city.setPromptText("London / Miami / Prague");
 
-        grid.add(new Label("Country:"), 0, 0);
-        grid.add(country, 1, 0);
+        grid.add(new Label("State:"), 0, 0);
+        grid.add(state, 1, 0);
+
         grid.add(new Label("City:"), 0, 1);
         grid.add(city, 1, 1);
 
@@ -107,26 +102,21 @@ public class Controller implements Initializable {
         confirmButton.setDisable(true);
 
         // Do some validation (using the Java 8 lambda syntax).
-        country.textProperty().addListener((observable, oldValue, newValue) -> {
+        state.textProperty().addListener((observable, oldValue, newValue) -> {
             confirmButton.setDisable(newValue.trim().isEmpty());
         });
 
         dialog.getDialogPane().setContent(grid);
 
-        // Request focus on the username field by default.
-        Platform.runLater(country::requestFocus);
-
         // Convert the result to a username-password-pair when the login button is clicked.
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == loginButtonType) {
-                return new Pair<>(country.getText(), city.getText());
+                return new Pair<>(state.getText(), city.getText());
             }
             return null;
         });
 
-        Optional<Pair<String, String>> result = dialog.showAndWait();
-
-        return result;
+        return dialog.showAndWait();
     }
 
     private void showWeatherData(WundergroundProvider wunderground) {
